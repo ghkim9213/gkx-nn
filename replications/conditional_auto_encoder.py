d374864b021546dcefd501a397612139959e0aae
gkim <gkim9213@gmail.com>
2024-01-08 16:31:51 +0900

    feat: add mlflow and optuna

--- replications/conditional_auto_encoder.py
+++ replications/conditional_auto_encoder.py
@@ -1,57 +1,86 @@
+from tqdm import tqdm
+
+import optuna
+import mlflow
 import torch
 from torch import nn
 from torch.utils.data import DataLoader
 
 from gkx_nn.data import GKXDatasetFactory, normalize, minmax
+from gkx_nn.experiment import train_epoch, validate_epoch
 from gkx_nn.models import ConditionalAutoEncoder
+from gkx_nn.loss_functions import LassoLoss
 
-# setup
+# device
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+# data
 ROOT_DIR = "./data"
-
+BATCH_SIZE = 10000
 ds_factory = GKXDatasetFactory(root_dir=ROOT_DIR)
 ds_factory.download_data()
 tvtsets = ds_factory.split_by_year(
     split_ratio=[.3, .2, .5],
-    from_year="2018",
-    # to_year="1961",
+    from_year="2011",
+    to_year="2020",
     scaling_func=normalize,
 )
-trainloader, validloader, testloader = (DataLoader(ds, batch_size=32, shuffle=True) for ds in tvtsets)
-model = ConditionalAutoEncoder().double()
-# import pdb; pdb.set_trace()
+trainloader, validloader, testloader = (DataLoader(ds, batch_size=BATCH_SIZE, shuffle=True) for ds in tvtsets)
+input_extractor = lambda loaded: loaded[:2]
+label_extractor = lambda loaded: loaded[2]
 
-# train
+# model
+model = ConditionalAutoEncoder(
+    batchnorm=True # Algorithm 7, GKX2020
+).double()
+
+# trainer
+# constant configs
+SEED = 0
 MAX_EPOCH = 10
-LEARNING_RATE = .001
-L1_PENALTY = .001
+PATIENCE = 5
 
-model.train()
-optimizer = torch.optim.Adam(model.parameters(), lr=LEARNING_RATE)
-loss_fn = nn.MSELoss()
+# hp space
+def suggest_hp(trial):
+    return (
+        trial.suggest_categorical("lr", [1e-3, 1e-2]),
+        trial.suggest_float("l1", 1e-5, 1e-3, log=True),
+    )
 
-size = len(trainloader.dataset)
-for batch, (x0, x1, y) in enumerate(trainloader):
-    pred = model(x0, x1)
-    loss = loss_fn(pred, y)
-    loss = loss + L1_PENALTY * sum(torch.norm(param.data) for nm, param in model.named_parameters())
+def objective(trial):
+    best_valid_loss = float("Inf")
+    patience = 0
+    lr, l1 = suggest_hp(trial)
+    print(f"[Trial {trial.number}] lr: {lr:>7f}, l1: {l1:>7f}")
+    with mlflow.start_run():
+        optimizer = torch.optim.Adam(model.parameters(), lr=lr)
+        loss_fn = LassoLoss(model.named_parameters(), l1=l1)
+        for epoch in range(MAX_EPOCH):
+            torch.manual_seed(SEED)
+            print(f"[Trial {trial.number} / epoch {epoch}] training...")
+            train_loss = train_epoch(
+                model, device, trainloader, optimizer, loss_fn,
+                input_extractor=input_extractor,
+                label_extractor=label_extractor,
+            )
+            print(f"[Trial {trial.number} / epoch {epoch}] validating...")
+            valid_loss = validate_epoch(
+                model, device, validloader, loss_fn,
+                input_extractor=input_extractor,
+                label_extractor=label_extractor,
+            )
+            if valid_loss <= best_valid_loss:
+                best_valid_loss = valid_loss
+            else:
+                patience += 1
+            
+            mlflow.log_metric("train_loss", train_loss, step=epoch)
+            mlflow.log_metric("valid_loss", valid_loss, step=epoch)
+            print(f"[Trial {trial.number} / epoch {epoch}] train_loss {train_loss:>7f}, valid_loss {valid_loss:7f}")
+            if patience == PATIENCE:
+                print(f"[Trial {trial.number} / epoch {epoch}] early stopped")
+                break
+    return best_valid_loss
 
-    optimizer.zero_grad()
-    loss.backward()
-    optimizer.step()
-    if batch % 1000 == 0:
-        loss, current = loss.item(), (batch+1) * len(x0)
-        print(f"loss: {loss:>7f}  [{current:>5d}/{size:>5d}]")
-
-# validate
-size = len(validloader.dataset)
-num_batches = len(validloader)
-test_loss, correct = 0, 0
-
-with torch.no_grad():
-    for x0, x1, y in validloader:
-        pred = model(x0, x1)
-        test_loss += loss_fn(pred, y).item()
-
-test_loss /= num_batches
-# correct /= size
-print(f"Test Error: Avg loss: {test_loss:>8f} \n")
+study = optuna.create_study(study_name="init_train", direction="minimize")
+study.optimize(objective, n_trials=5)
